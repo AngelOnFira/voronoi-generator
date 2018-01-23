c7f88f75af4d713e4e12bfe014aaa971b9f9b3f4
Angelonfira <wizcardforest@gmail.com>
2018-01-23 02:23:25 -0500

    Working spawner

---
+++ sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(800, 800);
+  var generators = [];
+
+  for (var i = 0; i < 30; i++) {
+    var color = [parseInt(Math.random() * 180 + 30), parseInt(Math.random() * 180 + 30), parseInt(Math.random() * 180 + 30)];
+    var pos = [parseInt(Math.random() * 800), parseInt(Math.random() * 800)];
+    var comb = [pos, color];
+    generators.push(comb);
+  }
+
+  for (var i = 0; i < 800; i++) {
+    for (var j = 0; j < 800; j++) {
+      var min = 0;
+      var dist_min = distance(j, generators[0][0][0], i, generators[0][0][1]);
+      for (var iter = 0; iter < generators.length; iter++) {
+        var dist_curr = distance(j, generators[iter][0][0], i, generators[iter][0][1]);
+        if (dist_curr < dist_min) {
+          min = iter;
+          var dist_min = distance(j, generators[min][0][0], i, generators[min][0][1]);
+        }
+      }
+      stroke(generators[min][1][0], generators[min][1][1], generators[min][1][2]);
+      point(j, i);
+    }
+  }
+}
+
+function draw() {
+
+}
+
+function distance(x1, x2, y1, y2) {
+  return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
+}
